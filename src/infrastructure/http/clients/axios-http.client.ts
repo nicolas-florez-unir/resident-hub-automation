651e0a8aa2d68248138eca84a7c442d359afcd09
nflorez <nflorez@ubits.co>
2025-04-05 15:15:40 -0500

    feat: fix typo

--- src/infrastructure/http/clients/axios-http.client.ts
+++ src/infrastructure/http/clients/axios-http.client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const axiosHttpClient = axios.create({
   baseURL: process.env.PRIVATE_API_URL,
   headers: {
-    "x-private-api-secret": process.env.PRIVATE_API_KEY,
+    "x-private-api-secret": process.env.PRIVATE_API_SECRET,
   },
 });
